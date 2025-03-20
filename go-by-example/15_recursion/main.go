0919498ae4c3111034f02331bd1f1ced63f99f7d
abdulhp <hpabdul902@gmail.com>
2025-03-20 14:10:59 +0700

    15_recursion

---
+++ go-by-example/15_recursion/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// math calculation : 6 x 5 x 4 x 3 x 2 x 1
+// This is recursive function
+func fact(n int) int {
+    if n == 0 {
+        return 1
+    }
+    return n * fact(n-1)
+}
+
+func main() {
+    fmt.Println(fact(7))
+
+    var fib func(n int) int
+
+	// this also recursive closure 
+    fib = func(n int) int {
+        if n < 2 {
+            return n
+        }
+
+        return fib(n-1) + fib(n-2)
+    }
+
+    fmt.Println(fib(7))
+}
